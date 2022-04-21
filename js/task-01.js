1a1656deff2f9eefb5af3a32762f38340211b33e
ira-panchyshyn <92648364+ira-panchyshyn@users.noreply.github.com>
2022-04-21 22:56:35 +0300

    added 2 exercises

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const listElement = document.querySelector("ul#categories")
+console.log(listElement);
+const listItems = listElement.querySelectorAll("li.item")
+console.log(`Number of categories: ${listItems.length}`);
+console.log();
+for (const item of listItems) {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.querySelectorAll("li").length}`);
+    console.log();
+}
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const items = ingredients.map(ingredient => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  return listItem
+})
+
+const list = document.querySelector("ul#ingredients");
+list.append(...items);
+
+
